e3e018db8177dc5060f430b41a08496f78d1556c
Le Thanh Tan Nhat <lethanhtannhat@gmail.com>
2026-05-13 15:26:56 +0700

    luthongnguyen

--- testime.py
+++ testime.py
@@ -59,11 +59,11 @@ def generate_precise_time_csv(file_name, n, start_str, end_str, start_h_limit, e
         print(f"Thành công! Đã tạo {n} hàng vào file '{file_name}'.")
 
 # --- CẤU HÌNH ---
-N_ROWS = 313
+N_ROWS = 250
 # Ngày 20 bắt đầu từ 18:00
-START_POINT = "05/09/2026 13:27" 
+START_POINT = "05/13/2026 15:20" 
 # Ngày 26 kết thúc lúc 23:59 để lấy được các giờ tối ngày 26
-END_POINT = "05/11/2026 21:59"    
+END_POINT = "05/22/2026 21:59"    
 
 # Giới hạn khung giờ hoạt động mỗi ngày
 LIMIT_START_H = 7
